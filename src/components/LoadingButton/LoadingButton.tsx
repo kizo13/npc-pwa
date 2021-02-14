51d0299d3a3380e9279f93ced255436dbe789fca
zkilvinger <zkilvinger@precognox.com>
2021-02-14 08:39:51 +0100

    refact LoadingButton

--- src/components/LoadingButton/LoadingButton.tsx
+++ src/components/LoadingButton/LoadingButton.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import Button, { ButtonProps } from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { withStyles, Theme, WithStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const styles = (theme: Theme) => ({
+const useStyles = makeStyles((theme) => ({
+  button: {
+    textTransform: 'initial',
+  },
   spinner: {
     marginRight: theme.spacing(1),
     color: '#286655',
   },
-});
+}));
 
-const SpinnerAdornment = withStyles(styles)(({ classes }: WithStyles<typeof styles>) => (
-  <CircularProgress
-    className={classes.spinner}
-    size={20}
-  />
-));
+const SpinnerAdornment = () => {
+  const classes = useStyles();
+  return (
+    <CircularProgress
+      className={classes.spinner}
+      size={20}
+    />
+  );
+};
 
 interface LoadingButtonProps {
   children: React.ReactNode,
   loading: boolean
 }
 
-const LoadingButton = ({ children, loading, ...rest }: LoadingButtonProps & ButtonProps): JSX.Element => (
-  <Button {...rest}>
-    {loading && <SpinnerAdornment />}
-    {children}
-  </Button>
-);
+const LoadingButton = ({ children, loading, ...rest }: LoadingButtonProps & ButtonProps): JSX.Element => {
+  const classes = useStyles();
+  return (
+    <Button className={classes.button} {...rest}>
+      {loading && <SpinnerAdornment />}
+      {children}
+    </Button>
+  );
+};
 
 export default LoadingButton;
