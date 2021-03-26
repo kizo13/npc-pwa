1cb0c7f0ffec3d08fc169a89a7847dcbacaf8b5e
zkilvinger <zkilvinger@precognox.com>
2021-03-26 23:50:58 +0100

    add custom button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MatButton, { ButtonProps } from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    textTransform: 'initial',
+  },
+}));
+
+const Button = ({ children, ...rest }: ButtonProps): JSX.Element => {
+  const classes = useStyles();
+  return <MatButton className={classes.button} {...rest}>{children}</MatButton>;
+};
+
+export default Button;
