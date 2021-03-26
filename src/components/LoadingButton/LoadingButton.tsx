5d3591d69b9844b4f434b47c5de147d0074f06e5
zkilvinger <zkilvinger@precognox.com>
2021-03-26 23:51:33 +0100

    update LoadingButton component with custom button

--- src/components/LoadingButton/LoadingButton.tsx
+++ src/components/LoadingButton/LoadingButton.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Button, { ButtonProps } from '@material-ui/core/Button';
+import { ButtonProps } from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '../Button';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    textTransform: 'initial',
-  },
   spinner: {
     marginRight: theme.spacing(1),
     color: '#286655',
@@ -24,18 +22,14 @@ const SpinnerAdornment = () => {
 };
 
 interface LoadingButtonProps {
-  children: React.ReactNode,
   loading: boolean
 }
 
-const LoadingButton = ({ children, loading, ...rest }: LoadingButtonProps & ButtonProps): JSX.Element => {
-  const classes = useStyles();
-  return (
-    <Button className={classes.button} {...rest}>
-      {loading && <SpinnerAdornment />}
-      {children}
-    </Button>
-  );
-};
+const LoadingButton = ({ children, loading, ...rest }: LoadingButtonProps & ButtonProps): JSX.Element => (
+  <Button {...rest}>
+    {loading && <SpinnerAdornment />}
+    {children}
+  </Button>
+);
 
 export default LoadingButton;
