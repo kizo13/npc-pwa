43de1698899ffe1e81cb426eb27b39237bb70e9c
zkilvinger <zkilvinger@precognox.com>
2021-03-26 18:29:02 +0100

    update login screen input with visibility icon

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Snackbar from '@material-ui/core/Snackbar';
 import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
 import Alert from '@material-ui/lab/Alert';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { AxiosError } from 'axios';
@@ -13,7 +18,14 @@ import { useUserContext } from '../../contexts/userContext';
 import { RouteWithLocation } from '../../shared/types/route-with-location.type';
 import { LoginResponseDto } from '../../shared/dtos/api-responses.dto';
 
+const useStyles = makeStyles(() => ({
+  input: {
+    width: 300,
+  },
+}));
+
 const Login: React.FunctionComponent<RouteWithLocation<{}>> = ({ location }) => {
+  const classes = useStyles();
   const { t } = useTranslation();
   const { setUser } = useUserContext();
   const history = useHistory();
@@ -21,6 +33,7 @@ const Login: React.FunctionComponent<RouteWithLocation<{}>> = ({ location }) => 
   const [password, setPassword] = useState('');
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [pending, setPending] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -58,11 +71,20 @@ const Login: React.FunctionComponent<RouteWithLocation<{}>> = ({ location }) => 
     setSnackbarOpen(false);
   };
 
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" height="100%">
       <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" m={1}>
         <Logo width="150" height="150" fill="#286655" />
         <TextField
+          className={classes.input}
           variant="outlined"
           size="small"
           id="email"
@@ -73,15 +95,30 @@ const Login: React.FunctionComponent<RouteWithLocation<{}>> = ({ location }) => 
           margin="normal"
         />
         <TextField
+          className={classes.input}
           variant="outlined"
           size="small"
           id="password"
-          type="password"
+          type={showPassword ? 'text' : 'password'}
           label={t('common.labels.password')}
           value={password}
           onChange={handlePasswordChange}
           onKeyDown={handleKeyDown}
           margin="normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
         <LoadingButton
           loading={pending}
